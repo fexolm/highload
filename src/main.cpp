580a3a22270e11b146942ede5bd4c7472cbb00dc
Alekseev Artem <fexolm@gmail.com>
2018-12-30 23:42:54 +0300

    json replace

--- src/main.cpp
+++ src/main.cpp
@@ -18,19 +18,16 @@
 
 #include "Request.h"
 
-char reply[] =
+std::string reply =
     "HTTP/1.1 200 OK\n"
     "Content-Type: application/json\n"
-    "Content-Length: 15\n"
     "Connection: keep-alive\n"
-    "\n"
-    "sdfkjsdnbfkjbsf";
+    "Content-Length: ";
 
 const int new_buf_size = 1024 * 512;
 const int turn_on = 1;
 const unsigned int thread_nums = 1;
-const size_t kReadBufferSize = 2048;
-const int MAX_IOVEC_PART = 1000;
+const size_t read_buffer_size = 2048;
 const int max_events = 1000;
 
 static inline int start_listen() {
@@ -42,14 +39,14 @@ static inline int start_listen() {
     exit(1);
   }
 
-  setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (void *)&turn_on, sizeof(turn_on));
-  setsockopt(listener, IPPROTO_TCP, TCP_QUICKACK, (void *)&turn_on, sizeof(turn_on));
+  setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (void *) &turn_on, sizeof(turn_on));
+  setsockopt(listener, IPPROTO_TCP, TCP_QUICKACK, (void *) &turn_on, sizeof(turn_on));
 
   addr.sin_family = AF_INET;
   addr.sin_port = htons(PORT);
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-  if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+  if (bind(listener, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
     perror("bind");
     exit(2);
   }
@@ -63,7 +60,7 @@ static inline std::vector<std::thread> start_workers(int epollfd) {
   for (unsigned int i = 0; i < thread_nums; ++i) {
     threads.emplace_back([epollfd]() {
       epoll_event events[max_events];
-      char buf[kReadBufferSize * 10];
+      char buf[read_buffer_size * 10];
       int nfds = 0;
       while (true) {
         nfds = epoll_wait(epollfd, events, max_events, 0);
@@ -78,26 +75,30 @@ static inline std::vector<std::thread> start_workers(int epollfd) {
           int sock = events[n].data.fd;
 
           size_t data_len = 0;
-          while(auto len = read(sock, buf + data_len, kReadBufferSize)) {
-            if(len < 0)
+          while (auto len = read(sock, buf + data_len, read_buffer_size)) {
+            if (len < 0)
               break;
             data_len += len;
           }
 
-          if(data_len == 0) {
+          if (data_len == 0) {
             continue;
           }
 
-          try {
-            hl::parse_request(boost::string_ref(buf, data_len));
-          } catch (...) {
-            std::cout << buf << std::endl;
+          auto req = hl::parse_request(boost::string_ref(buf, data_len));
+          auto response = reply;
+          if (req.method == hl::RequestMethod::POST) {
+            auto body = req.body.to_string();
+            std::replace(body.begin(), body.end(), '\n', ' ');
+            std::replace(body.begin(), body.end(), '\r', ' ');
+            response += std::to_string(req.body.length()) + "\n\n" + body;
+          } else {
+            response += "adsfasdfasdfasdfasdf";
           }
-//          if(req.method == hl::RequestMethod::POST) {
-//            std::cout << req.body << std::endl;
-//          }
 
-          write(sock, &reply, MAX_IOVEC_PART);
+          std::cout << response << std::endl;
+
+          write(sock, response.data(), response.size());
 
           epoll_event ev{};
           ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
@@ -122,13 +123,13 @@ static inline void main_loop(int listener, int epollfd) {
     }
 
     setsockopt(sock, SOL_SOCKET, SO_SNDBUF,
-               (void *)&new_buf_size, sizeof(new_buf_size));
+               (void *) &new_buf_size, sizeof(new_buf_size));
     setsockopt(sock, SOL_SOCKET, SO_RCVBUF,
-               (void *)&new_buf_size, sizeof(new_buf_size));
-    setsockopt(sock, SOL_SOCKET, SO_DONTROUTE, (void *)&turn_on, sizeof(turn_on));
-    setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (void *)&turn_on, sizeof(turn_on));
-    setsockopt(sock, SOL_SOCKET, SOCK_NONBLOCK, (void *)&turn_on, sizeof(turn_on));
-    int flags = fcntl(sock,F_GETFL,0);
+               (void *) &new_buf_size, sizeof(new_buf_size));
+    setsockopt(sock, SOL_SOCKET, SO_DONTROUTE, (void *) &turn_on, sizeof(turn_on));
+    setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (void *) &turn_on, sizeof(turn_on));
+    setsockopt(sock, SOL_SOCKET, SOCK_NONBLOCK, (void *) &turn_on, sizeof(turn_on));
+    int flags = fcntl(sock, F_GETFL, 0);
     fcntl(sock, F_SETFL, flags | O_NONBLOCK);
     epoll_event ev{};
     ev.events = EPOLLIN | EPOLLET | EPOLLONESHOT;
