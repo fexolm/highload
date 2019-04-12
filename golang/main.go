006dabc42cfd0c2cf8e776f13e35b3797d09151f
Alekseev Artem <fexolm@gmail.com>
2019-04-12 20:10:44 +0300

    use async calls in golang app

--- golang/main.go
+++ golang/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,14 +10,9 @@ import (
 )
 
 func main() {
-	// create new http server with max async 20 goroutines
 	server := httpserver.NewServer("mytest", 3005).SetAsyncNum(20)
 
-	// handler sync http request
-	server.HandlerRequst("POST", "/sync", syncDemo)
-
-	// handler async http request
-	server.HandlerAsyncRequst("POST", "/async", asyncDemo)
+	server.HandlerAsyncRequst("GET", "/", asyncDemo)
 	
 	go func(){
 		if err := server.Start(); err != nil {
@@ -26,8 +20,6 @@ func main() {
 	    	}
 	}()
 
-	// you can stop server using Stop() method which could await completion for all requests
-	// finishing off some extra-works by a system signal is recommended
 	EndChannel := make(chan os.Signal)
 	signal.Notify(EndChannel, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
 	select {
@@ -38,19 +30,6 @@ func main() {
 	close(EndChannel)
 }
 
-// simple handler for sync request
-// get response data immediately
-func syncDemo(jsonIn []byte) (jsonOut []byte, err error) {
-	log.Printf("[syncDemo] jsonIn: %v", string(jsonIn[:]))
-
-	return jsonIn, nil
-}
-
-// simple handler for async request
-// return task info in response data when performing request handler asynchronously
 func asyncDemo(jsonIn []byte) (err error) {
-	time.Sleep(5 * time.Second)
-	log.Printf("[asyncDemo] jsonIn: %v", string(jsonIn[:]))
-
 	return nil
 }
